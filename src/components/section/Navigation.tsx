cdc4e57dcc65ccc84a2917b3d3cc97675905d5bc
VasilZhelev <151950757+VasilZhelev@users.noreply.github.com>
2025-02-21 22:28:13 +0200

    small fix

--- src/components/section/Navigation.tsx
+++ src/components/section/Navigation.tsx
@@ -1,158 +1,18 @@
 "use client";
 
-import * as React from "react";
-import {NavLink as Link} from "react-router";
-
-import { cn } from "@/lib/utils";
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/nav/NavigationMenu";
-
 import {
   Navbar as NavbarComponent,
   NavbarLeft,
   NavbarRight,
 } from "@/components/ui/nav/NavBar";
-import {NavButton as Button} from "@/components/ui/nav/NavButton";
-import {Sheet, SheetTrigger, SheetContent} from '@/components/ui/nav/Sheet';
+import { NavButton as Button } from "@/components/ui/nav/NavButton";
+import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/nav/Sheet";
 
 import { Menu } from "lucide-react";
 import Logo from "@/assets/logo/default";
 
-const components: { title: string; href: string; description: string }[] = [
-  {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
-    description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
-  },
-  {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
-    description:
-      "For sighted users to preview content available behind a link.",
-  },
-  {
-    title: "Progress",
-    href: "/docs/primitives/progress",
-    description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-  },
-  {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
-  },
-  {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
-    description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
-  },
-  {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
-    description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
-  },
-];
+import {MenuBar} from '@/components/section/MenuBar';
 
-function NavBar() {
-  return (
-    <NavigationMenu className="hidden md:flex">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/30 to-muted/10 p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <Logo />
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      Launch UI
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Landing page template built with React, Shadcn/ui and
-                      Tailwind that you can copy/paste into your project.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-              {components.map((component) => (
-                <ListItem
-                  key={component.title}
-                  title={component.title}
-                  href="/"
-                >
-                  {component.description}
-                </ListItem>
-              ))}
-            </ul>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link to="/">
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-              Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
-  );
-}
-
-const ListItem = React.forwardRef<
-  React.ElementRef<"a">,
-  React.ComponentPropsWithoutRef<"a">
->(({ className, title, children, ...props }, ref) => {
-  return (
-    <li>
-      <NavigationMenuLink asChild>
-        <a
-          ref={ref}
-          className={cn(
-            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-            className,
-          )}
-          {...props}
-        >
-          <div className="text-sm font-medium leading-none">{title}</div>
-          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
-            {children}
-          </p>
-        </a>
-      </NavigationMenuLink>
-    </li>
-  );
-});
-ListItem.displayName = "ListItem";
 
 
 function Navigation() {
@@ -162,14 +22,11 @@ function Navigation() {
       <div className="relative mx-auto max-w-container">
         <NavbarComponent>
           <NavbarLeft>
-            <a
-              href="/"
-              className="flex items-center gap-2 text-xl font-bold"
-            >
+            <a href="/" className="flex items-center gap-2 text-xl font-bold">
               <Logo />
               Launch UI
             </a>
-            <NavBar />
+            <MenuBar />
           </NavbarLeft>
           <NavbarRight>
             <a href="/" className="hidden text-sm md:block">
@@ -180,39 +37,23 @@ function Navigation() {
             </Button>
             <Sheet>
               <SheetTrigger asChild>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="shrink-0 md:hidden"
-                >
+                <Button variant="ghost" size="icon" className="shrink-0 md:hidden">
                   <Menu className="h-5 w-5" />
                   <span className="sr-only">Toggle navigation menu</span>
                 </Button>
               </SheetTrigger>
               <SheetContent side="right">
                 <nav className="grid gap-6 text-lg font-medium">
-                  <a
-                    href="/"
-                    className="flex items-center gap-2 text-xl font-bold"
-                  >
+                  <a href="/" className="flex items-center gap-2 text-xl font-bold">
                     <span>Launch UI</span>
                   </a>
-                  <a
-                    href="/"
-                    className="text-muted-foreground hover:text-foreground"
-                  >
+                  <a href="/" className="text-muted-foreground hover:text-foreground">
                     Getting Started
                   </a>
-                  <a
-                    href="/"
-                    className="text-muted-foreground hover:text-foreground"
-                  >
+                  <a href="/" className="text-muted-foreground hover:text-foreground">
                     Components
                   </a>
-                  <a
-                    href="/"
-                    className="text-muted-foreground hover:text-foreground"
-                  >
+                  <a href="/" className="text-muted-foreground hover:text-foreground">
                     Documentation
                   </a>
                 </nav>
