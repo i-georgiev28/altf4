bbd4764651e6b33c62864926a3edf842990f4307
VasilZhelev <151950757+VasilZhelev@users.noreply.github.com>
2025-02-22 13:52:29 +0200

    Added Features and About us

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,8 @@ import tailwindAnimate from "tailwindcss-animate";
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: ["class"],
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     container: {
       center: true,
