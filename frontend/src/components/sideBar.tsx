629013a8f9f1298681a4b8316f034a8fdefc4f7d
VasilZhelev <151950757+VasilZhelev@users.noreply.github.com>
2025-02-23 11:26:23 +0200

    Fix username

--- frontend/src/components/sideBar.tsx
+++ frontend/src/components/sideBar.tsx
@@ -1,9 +1,11 @@
 import { Layers, CircleGauge, LayoutPanelTop, Settings } from "lucide-react";
 
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect, useState } from 'react';
 
 import { Sidebar as SideBarComponent, SidebarBody, SidebarLink } from "@/components/ui/sidebar";
 import Logo from "@/assets/Logo";
+import api from "@/lib/api";
+import { useNavigate } from "react-router";
 
 interface SideBarProps {
     isOpen: boolean;
@@ -11,13 +13,28 @@ interface SideBarProps {
   }
 
 const SideBar: React.FC<SideBarProps> = ({ isOpen, setOpen }) => {
+
+  const [username, setUsername] = useState<string>("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      api
+        .get("/auth/me")
+        .then((response) => setUsername(response.data.username))
+        .catch((error) => console.log(error));
+    };
+
+    fetchUserData();
+  }, [navigate]);
+
     return (
         <SideBarComponent open={isOpen} setOpen={setOpen}>
         <SidebarBody className="ml-10 bg-gray-100 justify-between gap-10">
           <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
             <div className="flex flex-row">
               <Logo className="w-7 h-7 block"/>
-              {isOpen ? <h1 className="inline">Logo</h1> : <></>}
+              {isOpen ? <h1 className="inline">SolarEye</h1> : <></>}
             </div>
             <div className="mt-8 flex flex-col gap-2">
               {links.map((link, idx) => (
@@ -28,11 +45,11 @@ const SideBar: React.FC<SideBarProps> = ({ isOpen, setOpen }) => {
           <div>
             <SidebarLink
               link={{
-                label: "Manu Arora",
+                label: username,
                 href: "#",
                 icon: (
                   <img
-                    src="https://assets.aceternity.com/manu.png"
+                    src="https://aui.atlassian.com/aui/8.8/docs/images/avatar-person.svg"
                     className="h-7 w-7 flex-shrink-0 rounded-full"
                     width={50}
                     height={50}
@@ -53,7 +70,7 @@ const links = [
       label: "Dashboard",
       href: "#",
       icon: (
-        <Layers className="text-neutral-700 h-6 w-6 flex-shrink-0" />
+        <Layers className="text-neutral h-6 w-6 flex-shrink-0" />
       ),
     },
     {
